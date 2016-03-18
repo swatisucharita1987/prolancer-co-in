a33b869afc7e586d4d27e82175a0bac609cfb784
swati <khusi.swati@gmail.com>
2016-03-17 22:59:25 -0700

    Cards-Landing page

--- prolance-app/app/controllers/sessions_controller.rb
+++ prolance-app/app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by(user_name: params[:user_name])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
+      session[:user_name] = user.user_name
       redirect_to '/welcome/landing_page'
     else 
       redirect_to login_url, alert: "Incorrect user name and password"
